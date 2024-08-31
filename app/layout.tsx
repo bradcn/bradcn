4fe683d7eb7f43baff95212a5968fddc5a41f956
lisaorg <amol.yadav.778@gmail.com>
2024-08-31 14:51:49 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { Footer } from "@/components/footer";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "NexDocx template",
-  metadataBase: new URL("https://nexdocs.vercel.app//"),
+  title: "LISA Org",
+  metadataBase: new URL("https://lisaorg.vercel.app/"),
   description:
-    "This comprehensive documentation template, crafted with Next.js and available as open-source, delivers a sleek and responsive design, tailored to meet all your project documentation requirements.",
+    "LISA stands for Libraries and Indexed Software Assets is a vast library or software system delivering UI components, CSS, themes, templates and many more.",
 };
 
 export default function RootLayout({
