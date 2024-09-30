c03af9bf7cc8dcd5987beea2e22e3c0668263ebd
lisaorg <amol.yadav.778@gmail.com>
2024-09-30 18:37:49 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -88,38 +88,13 @@ export function Footer() {
     ]
 
     return (
-        <footer className="pt-10 bg-gray-800">
+        <footer className="pt-10">
             <div className="max-w-screen-xl mx-auto px-4 md:px-8">
-                <div className="justify-between items-center gap-12 md:flex">
-                    <div className="flex-1 max-w-lg">
-                        <h3 className="text-white text-2xl font-bold">
-                            Get our beautiful newsletter straight to your inbox.
-                        </h3>
-                    </div>
-                    <div className="flex-1 mt-6 md:mt-0">
-                        <form onSubmit={(e) => e.preventDefault()} className="flex items-center gap-x-3 md:justify-end">
-                            <div className="relative">
-                                <svg className="w-6 h-6 text-gray-400 absolute left-3 inset-y-0 my-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-                                </svg>
-                                <input
-                                    type="email"
-                                    required
-                                    placeholder="Enter your email"
-                                    className="w-full pl-12 pr-3 py-2 text-gray-500 bg-white outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                                />
-                            </div>
-                            <button className="block w-auto py-3 px-4 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 active:shadow-none rounded-lg shadow">
-                                Subscribe
-                            </button>
-                        </form>
-                    </div>
-                </div>
                 <div className="flex-1 mt-16 space-y-6 justify-between sm:flex md:space-y-0">
                     {
                         footerNavs.map((item, idx) => (
                             <ul
-                                className="space-y-4 text-gray-300"
+                                className="space-y-4"
                                 key={idx}
                             >
                                 <h4 className="text-gray-200 font-semibold sm:pb-2">
@@ -130,7 +105,7 @@ export function Footer() {
                                         <li key={idx}>
                                             <Link
                                                 href={el.href}
-                                                className="duration-150 hover:text-gray-400"
+                                                className="duration-150"
 
                                             >
                                                 {el.name}
@@ -143,7 +118,7 @@ export function Footer() {
                     }
                 </div>
                 <div className="mt-10 py-10 border-t border-gray-700 items-center justify-between sm:flex">
-                    <p className="text-gray-300">© 2022 Float UI Inc. All rights reserved.</p>
+                    <p className="">© 2024 Bradcn Inc. All rights reserved.</p>
                     <div className="flex items-center gap-x-6 text-gray-400 mt-6">
                         <a href="javascript:void()">
                             <svg className="w-6 h-6 hover:text-gray-500 duration-150" fill="none" viewBox="0 0 48 48"><g clip-path="url(#a)"><path fill="currentColor" d="M48 24C48 10.745 37.255 0 24 0S0 10.745 0 24c0 11.979 8.776 21.908 20.25 23.708v-16.77h-6.094V24h6.094v-5.288c0-6.014 3.583-9.337 9.065-9.337 2.625 0 5.372.469 5.372.469v5.906h-3.026c-2.981 0-3.911 1.85-3.911 3.75V24h6.656l-1.064 6.938H27.75v16.77C39.224 45.908 48 35.978 48 24z" /></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h48v48H0z" /></clipPath></defs></svg>
